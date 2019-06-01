a50849468be9008409a5c612a6e413bec46b238c
vineelTeja <tejavine@gmail.com>
2019-05-31 20:58:56 -0700

    1. Static files like CSS goes to project_file / static         1.1 To include static files use this: {{ url_for('static', filename = 'main.css') }}             1.1.1 url_for() method is imported from flask in .py file.
        2. Templates files goes to project_files / templates
            2.1 To implement render_template use this render_template('home.html', posts = posts, title = 'Home Page')
                2.1.1 Posts & Title in args are passed on to the templates as variables.

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'person 1',
+        'title': 'title 1',
+        'content': 'major content',
+        'date_posted': 'Fri 30 May 2010'
+    },
+    {
+        'author': 'person 222',
+        'title': 'title 222',
+        'content': 'major content 22',
+        'date_posted': 'Fri 30 JUN 22'
+    },
+    {
+        'author': 'person 333',
+        'title': 'title 333',
+        'content': 'major content 33',
+        'date_posted': 'Fri 30 FEB 20133'
+    }
+]
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html', posts = posts, title = 'Home Page')
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About page')
+
 
 
 if __name__ == '__main__':
